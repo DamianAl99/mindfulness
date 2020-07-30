a558a04ea46af5741e78ee45233baa7b81737183
Damian <damiianalmada06@gmail.com>
2020-07-30 16:43:42 -0400

    completo ya

--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +11,9 @@ export class ClimaServices {
 
   constructor(private http:HttpClient) { 
     this.uri = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=asuncion,py&appid=${this.apiKey}`
-  }
+  };
 
   getClima(){
     return this.http.get(this.uri)
-  }
+  };
 }
